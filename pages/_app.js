b208c189c89b0cf1288059a036ce0e2075c84bc0
Priyanshu Sharma <inbox.priyanshu@gmail.com>
2023-03-05 00:13:28 +0530

    added intial dependenices

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Be_Vietnam_Pro } from "next/font/google";
+
+const vietnam = Be_Vietnam_Pro({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`bg-gray-100 h-screen ${vietnam.className}`}>
+      <main className="max-w-lg mx-auto bg-white h-full">
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
+import { Marcellus } from "next/font/google";
+
+const marcellus = Marcellus({
+  weight: "400",
+  subsets: ["latin"],
+});
+
 export default function Home() {
   return (
-    <div className="text-xl font-extrabold">
-      <p>Hello Next.js</p>
+    <div className={`text-xl font-extrabold text-black ${marcellus.className}`}>
+      <h1>
+        Test <br />
+        Test <br />
+        <span>Companies</span>
+      </h1>
     </div>
   );
 }
