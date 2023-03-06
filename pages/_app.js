494bd62290e4836f391c7c47f561e0cacbb9803a
Priyanshu Sharma <inbox.priyanshu@gmail.com>
2023-03-06 15:04:20 +0530

    fixed app.js

--- pages/_app.js
+++ pages/_app.js
@@ -8,15 +8,10 @@ const vietnam = Be_Vietnam_Pro({
 
 function MyApp({ Component, pageProps }) {
   return (
-<<<<<<< HEAD
-    <div className={`h-screen bg-gray-200 ${vietnam.className} overflow-hidden`}>
-      <main className="full-height relative mx-auto max-w-lg bg-white">
-=======
     <div
       className={`h-screen bg-gray-200 ${vietnam.className} overflow-hidden`}
     >
-      <main className="full-height relative mx-auto max-w-lg overflow-hidden bg-white">
->>>>>>> main
+      <main className="full-height relative mx-auto max-w-lg bg-white">
         <Component {...pageProps} />
       </main>
     </div>
