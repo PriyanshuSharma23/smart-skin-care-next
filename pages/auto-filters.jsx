3bec299c87a7f5b842a16e57c307c96c2369a85e
Priyanshu Sharma <inbox.priyanshu@gmail.com>
2023-03-07 17:17:19 +0530

    finished auto-filters

--- pages/auto-filters.jsx
+++ pages/auto-filters.jsx
@@ -5,6 +5,7 @@ import skinTones from "../public/assets/skinTones.jpg";
 import { gsap } from "gsap";
 import Webcam from "react-webcam";
 import Show from "../components/Show";
+import Link from "next/link";
 
 const vietnam = Be_Vietnam_Pro({
   weight: "400",
@@ -21,6 +22,7 @@ export default function Page() {
   const [isCameraShown, setIsCameraShown] = useState(false);
   const [cameraView, setCameraView] = useState("user");
   const [imageSrc, setImageSrc] = useState(null);
+  const [dragging, setDragging] = useState(false);
 
   const [result, setResult] = useState(null);
 
@@ -52,6 +54,9 @@ export default function Page() {
   const heroRef = useRef(null);
   const cameraBoxRef = useRef(null);
   const cameraRef = useRef(null);
+  const dropzoneRef = useRef(null);
+
+  useEffect(() => {}, []);
 
   useEffect(() => {
     // add outside click
@@ -120,7 +125,18 @@ export default function Page() {
 
   return (
     <>
-      <div className="relative flex h-full flex-col">
+      <div
+        className="relative flex h-full flex-col"
+        ref={dropzoneRef}
+        onDragEnter={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+
+          if (!dragging) {
+            setDragging(true);
+          }
+        }}
+      >
         <div className="flex-col items-start leading-tight tracking-wide">
           <div
             ref={heroRef}
@@ -144,108 +160,80 @@ export default function Page() {
                 isCameraShown ? "h-96" : "h-60"
               }`}
             >
-              {/* When Camera Shown */}
-              <Show when={isCameraShown} fallback={null}>
-                {/* When Image not clicked */}
-                <Show when={!imageSrc} fallback={null}>
-                  <Webcam
-                    imageSmoothing={true}
-                    className="h-full w-full rounded-md object-cover"
-                    ref={cameraRef}
-                    audio={false}
-                    screenshotFormat="image/jpeg"
-                    videoConstraints={{
-                      facingMode: cameraView,
-                    }}
-                  />
-                </Show>
+              <Show
+                when={result === null}
+                fallback={
+                  <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                    <svg
+                      className="checkmark addClass h-8 w-8"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 52 52"
+                    >
+                      <circle
+                        className="checkmark__circle addClass"
+                        cx="26"
+                        cy="26"
+                        r="25"
+                        fill="none"
+                      />
+                      <path
+                        className="checkmark__check addClass"
+                        fill="none"
+                        d="M14.1 27.2l7.1 7.2 16.7-16.8"
+                      />
+                    </svg>
+                  </div>
+                }
+              >
+                <>
+                  {/* When Camera Shown */}
+                  <Show when={isCameraShown} fallback={null}>
+                    {/* When Image not clicked */}
+                    <Show when={!imageSrc} fallback={null}>
+                      <Webcam
+                        imageSmoothing={true}
+                        className="h-full w-full rounded-md object-cover"
+                        ref={cameraRef}
+                        audio={false}
+                        screenshotFormat="image/jpeg"
+                        videoConstraints={{
+                          facingMode: cameraView,
+                        }}
+                      />
+                    </Show>
 
-                {/* When Image clicked */}
-                <Show when={imageSrc} fallback={null}>
-                  <Image
-                    src={imageSrc}
-                    fill="cover"
-                    alt="pic"
-                    className="h-full w-full rounded-md object-cover"
-                  />
-                </Show>
-              </Show>
-
-              {/* When Camera Not Shown */}
-              <Show when={!isCameraShown} fallback={null}>
-                <Show
-                  when={!loading}
-                  fallback={
-                    <div className="absolute top-0 left-0 z-20 h-full w-full rounded-md">
+                    {/* When Image clicked */}
+                    <Show when={imageSrc} fallback={null}>
                       <Image
-                        src={finalImage}
+                        src={imageSrc}
                         fill="cover"
                         alt="pic"
-                        className="fixed top-0 left-0 z-10 h-full w-full rounded-md object-cover "
+                        className="h-full w-full rounded-md object-cover"
                       />
-                      <div className="absolute top-0 left-0 z-20 h-full w-full rounded-md bg-black bg-opacity-50"></div>
-                      <div className="absolute top-1/2 left-1/2 z-30 flex -translate-x-1/2 -translate-y-1/2 gap-2">
-                        <div className="flex items-center justify-center">
-                          <div
-                            className="inline-block  h-8 w-8 animate-spin rounded-full border-4 border-solid border-white border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-                            role="status"
-                          >
-                            <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-                              Loading...
-                            </span>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  }
-                >
-                  {/* When a final image exists */}
-                  <Show when={finalImage} fallback={null}>
-                    <div className="relative h-full w-full ">
-                      <Image
-                        src={finalImage}
-                        fill="cover"
-                        alt="pic"
-                        className="fixed top-0 left-0 z-10 h-full w-full rounded-md object-cover "
-                      />
-                      <div className="absolute top-0 left-0 z-20 h-full w-full rounded-md bg-black bg-opacity-50"></div>
-
-                      <div className="absolute bottom-0 left-1/2 z-30 flex -translate-x-1/2 gap-2 pb-2">
-                        <button
-                          className="opacity-btn rounded-md border bg-black px-3 py-2 text-lg text-white"
-                          onClick={() => {
-                            setFinalImage(null);
-                          }}
-                        >
-                          Clear
-                        </button>
-
-                        <button
-                          className="opacity-btn rounded-md border bg-black px-3 py-2 text-lg text-white"
-                          onClick={() => {
-                            handleUpload();
-                          }}
-                        >
-                          Upload
-                        </button>
-                      </div>
-                    </div>
+                    </Show>
                   </Show>
 
-                  {/* When not a final image */}
-                  <Show when={!finalImage} fallback={null}>
-                    <div className="p-12">
-                      <p className="mb-4 text-2xl">Drop files to upload</p>
-                      <p className="text-1xl mb-4">or</p>
-                      <button
-                        className="opacity-btn flex-grow rounded-md border bg-black px-3 py-2 text-lg text-white"
-                        onClick={handleClick}
+                  {/* When Camera Not Shown */}
+                  <Show when={!isCameraShown} fallback={null}>
+                    <Show
+                      when={!loading}
+                      fallback={<Loading finalImage={finalImage} />}
+                    >
+                      <Show
+                        when={finalImage}
+                        fallback={
+                          <ImageSelectionPrompt handleClick={handleClick} />
+                        }
                       >
-                        Select Files
-                      </button>
-                    </div>
+                        <FinalConfirmation
+                          finalImage={finalImage}
+                          handleUpload={handleUpload}
+                          setFinalImage={setFinalImage}
+                        />
+                      </Show>
+                    </Show>
                   </Show>
-                </Show>
+                </>
               </Show>
             </div>
           </div>
@@ -253,12 +241,47 @@ export default function Page() {
             <Show
               when={isCameraShown}
               fallback={
-                <>
-                  <h2 className="mb-4 text-2xl">Description</h2>
-                  <p className="text-1xl">
-                    Detects any problems, race with one photo of yours
-                  </p>
-                </>
+                <Show
+                  when={result == null}
+                  fallback={
+                    <>
+                      <h2 className="mb-4 text-2xl">Detected</h2>
+                      <ul>
+                        <li className="text-lg">
+                          Skin Type:{" "}
+                          <span className="ml-2 font-bold text-neutral-600">
+                            {result?.skinType}
+                          </span>
+                          <Link
+                            className="ml-2 text-sm text-blue-500"
+                            href="/manual-filters/1"
+                          >
+                            Change
+                          </Link>
+                        </li>
+                        <li className="text-lg">
+                          Disease:
+                          <span className="ml-2 font-bold text-red-500">
+                            {result?.disease}
+                          </span>
+                          <Link
+                            className="ml-2 text-sm text-blue-500"
+                            href="/manual-filters/2"
+                          >
+                            Change
+                          </Link>
+                        </li>
+                      </ul>
+                    </>
+                  }
+                >
+                  <>
+                    <h2 className="mb-4 text-2xl">Description</h2>
+                    <p className="text-1xl">
+                      Detects any problems, race with one photo of yours
+                    </p>
+                  </>
+                </Show>
               }
             >
               <div className="flex w-full items-center justify-around gap-2">
@@ -380,7 +403,7 @@ export default function Page() {
 
         <div
           ref={popUpRef}
-          className="fixed bottom-0 left-1/2 z-20 h-80 w-full max-w-md -translate-x-1/2 flex-col items-center justify-center rounded-lg bg-white p-8 text-center shadow-2xl"
+          className="fixed bottom-0 left-1/2 z-50 h-80 w-full max-w-md -translate-x-1/2 flex-col items-center justify-center rounded-lg bg-white p-8 text-center shadow-2xl"
         >
           <p className="mb-4 text-lg">Choose files for upload</p>
           <button
@@ -395,14 +418,165 @@ export default function Page() {
             </svg>
             <p>Camera</p>
           </button>
-          <button className="flex h-1/3 w-full items-center justify-center gap-4 rounded-lg bg-gray-200 text-lg outline-dashed outline-gray-300">
+          <label
+            htmlFor="file-input"
+            className="flex h-1/3 w-full items-center justify-center gap-4 rounded-lg bg-gray-200 text-lg outline-dashed outline-gray-300"
+          >
             <svg viewBox="0 0 512 512" width="40px">
               <path d="M448 480H64c-35.3 0-64-28.7-64-64V192H512V416c0 35.3-28.7 64-64 64zm64-320H0V96C0 60.7 28.7 32 64 32H192c20.1 0 39.1 9.5 51.2 25.6l19.2 25.6c6 8.1 15.5 12.8 25.6 12.8H448c35.3 0 64 28.7 64 64z" />
             </svg>
             <p>Storage</p>
-          </button>
+            {/* hidden file input element */}
+            <input
+              id="file-input"
+              type="file"
+              className="hidden"
+              onChange={(e) => {
+                const file = e.target.files[0];
+                if (file) {
+                  const reader = new FileReader();
+                  reader.onload = (e) => {
+                    // check if file is image
+                    if (file.type.split("/")[0] !== "image") {
+                      alert("Please select an image file");
+                      return;
+                    }
+                    setFinalImage(e.target.result);
+                    setIsShown(false);
+                  };
+                  reader.readAsDataURL(file);
+                }
+              }}
+            />
+          </label>
         </div>
       </div>
+
+      <Show when={dragging}>
+        <div
+          className="
+          fixed top-0 left-0 z-50 flex h-full w-full items-center justify-center bg-white/50
+            backdrop-blur-md
+          "
+          onDragOver={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+
+            setDragging(true);
+          }}
+          onDragLeave={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+
+            setDragging(false);
+          }}
+          onDrop={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+
+            console.log(e.dataTransfer.files);
+
+            const files = e.dataTransfer.files;
+            if (files.length > 0) {
+              const file = files[0];
+              console.log(file);
+              if (!file.type.startsWith("image/")) {
+                return;
+              }
+              const reader = new FileReader();
+              reader.onload = (e) => {
+                setFinalImage(e.target.result);
+              };
+              reader.readAsDataURL(file);
+            }
+
+            setDragging(false);
+          }}
+        >
+          <h1
+            className="
+            text-2xl
+            "
+          >
+            Drop Images to upload
+          </h1>
+        </div>
+      </Show>
     </>
+  );
+}
+
+function ImageSelectionPrompt({ handleClick }) {
+  return (
+    <div className="p-12">
+      <p className="mb-4 text-2xl">Drop files to upload</p>
+      <p className="text-1xl mb-4">or</p>
+      <button
+        className="opacity-btn flex-grow rounded-md border bg-black px-3 py-2 text-lg text-white"
+        onClick={handleClick}
+      >
+        Select Files
+      </button>
+    </div>
+  );
+}
+
+function FinalConfirmation({ finalImage, setFinalImage, handleUpload }) {
+  return (
+    <div className="relative h-full w-full ">
+      <Image
+        src={finalImage}
+        fill="cover"
+        alt="pic"
+        className="fixed top-0 left-0 z-10 h-full w-full rounded-md object-cover "
+      />
+      <div className="absolute top-0 left-0 z-20 h-full w-full rounded-md bg-black bg-opacity-50"></div>
+
+      <div className="absolute bottom-0 left-1/2 z-30 flex -translate-x-1/2 gap-2 pb-2">
+        <button
+          className="opacity-btn rounded-md border bg-black px-3 py-2 text-lg text-white"
+          onClick={() => {
+            setFinalImage(null);
+          }}
+        >
+          Clear
+        </button>
+
+        <button
+          className="opacity-btn rounded-md border bg-black px-3 py-2 text-lg text-white"
+          onClick={() => {
+            handleUpload();
+          }}
+        >
+          Upload
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function Loading({ finalImage }) {
+  return (
+    <div className="absolute top-0 left-0 z-20 h-full w-full rounded-md">
+      <Image
+        src={finalImage}
+        fill="cover"
+        alt="pic"
+        className="fixed top-0 left-0 z-10 h-full w-full rounded-md object-cover "
+      />
+      <div className="absolute top-0 left-0 z-20 h-full w-full rounded-md bg-black bg-opacity-50"></div>
+      <div className="absolute top-1/2 left-1/2 z-30 flex -translate-x-1/2 -translate-y-1/2 gap-2">
+        <div className="flex items-center justify-center">
+          <div
+            className="inline-block  h-8 w-8 animate-spin rounded-full border-4 border-solid border-white border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+            role="status"
+          >
+            <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+              Loading...
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
